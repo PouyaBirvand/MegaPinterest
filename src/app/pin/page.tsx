9da2c6426343548d1d385b68b962773f6d247cf7
PouyaBirvand <pooyabirvand@gmail.com>
2025-06-12 21:18:03 +0330

    Change Arrow function to Regular function (pinCard)

--- src/app/pin/page.tsx
+++ src/app/pin/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
-const PinPage = () => {
+export default function PinPage() {
   const router = useRouter();
   return router.push('/');
 };
-export default PinPage;
